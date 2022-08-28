0aab7df69d9bea8369c94dd189f30942cb97545d
RianKhanafi <riankhanafi69@gmail.com>
2022-08-28 16:40:53 +0700

    layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../../styles/Home.module.css";
 
-export default function Layout({ children }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return <div className={styles.containerlayout}>{children}</div>;
 }
