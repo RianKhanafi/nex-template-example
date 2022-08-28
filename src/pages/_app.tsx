b774480d2fe93ef42e8780cca682baa759483e7a
RianKhanafi <riankhanafi69@gmail.com>
2022-08-28 17:23:32 +0700

    test error commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
-import "../../styles/globals.css";
-import { SessionProvider } from "next-auth/react";
-import type { AppProps } from "next/app";
+import '../../styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <SessionProvider session={pageProps.session} refetchInterval={0}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+	return (
+		<SessionProvider session={pageProps.session} refetchInterval={0}>
+			<Component {...pageProps} />
+		</SessionProvider>
+	);
 }
 
 export default App;
--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,6 +1,7 @@
-import styles from "../../styles/Home.module.css";
-import Layout from "../components/layout";
+import styles from '../../styles/Home.module.css';
+import Layout from '../components/layout';
 
 export default function Error() {
-  return <Layout>Error Page</Layout>;
+	console.loh;
+	return <Layout>Error Page</Layout>;
 }
